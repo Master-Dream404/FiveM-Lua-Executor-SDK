4c9bb8fbce1d2f0a7ce68d606996bc991248f79b
Master Dream <98493659+Master-Dream404@users.noreply.github.com>
2022-03-18 22:03:08 +0100

    Add files via upload

---
+++ ObjectModel/OMClass.h
@@ -0,0 +1,173 @@
+/*
+ * This file is part of the CitizenFX project - http://citizen.re/
+ *
+ * See LICENSE and MENTIONS in the root of the source tree for information
+ * regarding licensing.
+ */
+
+#pragma once
+
+#include <boost/preprocessor/comma_if.hpp>
+#include <boost/preprocessor/repeat.hpp>
+
+namespace fx
+{
+template<class... T> class OMClassParent;
+
+#define _DECLARE_CLASS_ENTRY(z, n, text) BOOST_PP_COMMA_IF(n) text T ## n
+
+#define _DECLARE_OM_PARENT_TEMPLATE(x) BOOST_PP_REPEAT(x, _DECLARE_CLASS_ENTRY, class)
+#define _DECLARE_OM_PARENT_BASES(x) BOOST_PP_REPEAT(x, _DECLARE_CLASS_ENTRY, public)
+#define _DECLARE_OM_PARENT_SPEC(x) BOOST_PP_REPEAT(x, _DECLARE_CLASS_ENTRY, )
+#define DECLARE_OM_PARENT(x) template<_DECLARE_OM_PARENT_TEMPLATE(x)> \
+	class OMClassParent<_DECLARE_OM_PARENT_SPEC(x)> : _DECLARE_OM_PARENT_BASES(x) { \
+		protected: \
+			fxIBase* GetBaseRef() { return static_cast<T0*>(this); } \
+	};
+
+DECLARE_OM_PARENT(1)
+DECLARE_OM_PARENT(2)
+DECLARE_OM_PARENT(3)
+DECLARE_OM_PARENT(4)
+DECLARE_OM_PARENT(5)
+DECLARE_OM_PARENT(6)
+DECLARE_OM_PARENT(7)
+DECLARE_OM_PARENT(8)
+
+template<class TClass, class... TInterface>
+class OMClass : public OMClassParent<TInterface...>
+{
+private:
+	class RefCount
+	{
+	private:
+		std::atomic<int32_t> m_count;
+
+	public:
+		RefCount()
+			: m_count(0)
+		{
+		}
+
+		inline std::atomic<int32_t>& GetCount()
+		{
+			return m_count;
+		}
+	};
+
+	struct pass
+	{
+		template<typename ...T> pass(T...) {}
+	};
+
+	RefCount m_refCount;
+
+protected:
+	OMClass() {}
+
+	virtual ~OMClass() {}
+
+private:
+	void* operator new(size_t size) noexcept
+	{
+		return nullptr;
+	}
+
+	void* operator new(size_t size, void* ptr)
+	{
+		return ptr;
+	}
+
+public:
+	template<typename TNewClass, typename... TArg>
+	friend OMPtr<TNewClass> MakeNew(TArg...);
+
+	template<typename TNewClass, typename... TArg>
+	friend fxIBase* MakeNewBase(TArg...);
+
+public:
+	virtual result_t QueryInterface(const guid_t& riid, void** outObject) override
+	{
+		result_t result = FX_E_NOINTERFACE;
+
+		pass{ ([&]
+		{
+			if (result == FX_E_NOINTERFACE)
+			{
+				if (riid == TInterface::GetIID())
+				{
+					result = FX_S_OK;
+					*outObject = static_cast<TInterface*>(this);
+
+					AddRef();
+				}
+			}
+		}(), 1)... };
+
+		if (result == FX_E_NOINTERFACE)
+		{
+			if (riid == guid_t{ 0x00000000, 0x0000, 0x0000,{ 0xc0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x46 } })
+			{
+				result = FX_S_OK;
+				*outObject = this;
+
+				AddRef();
+			}
+		}
+
+		return result;
+	}
+
+	virtual uint32_t AddRef() override
+	{
+#ifdef OM_REF_DEBUG
+		if (m_refCount.GetCount() == 0)
+		{
+			trace(__FUNCTION__ " ref'ing %p\n", this);
+		}
+#endif
+
+		return m_refCount.GetCount().fetch_add(1) + 1;
+	}
+
+	virtual uint32_t Release() override
+	{
+        auto c = m_refCount.GetCount().fetch_sub(1) - 1;
+
+		if (c <= 0)
+		{
+#ifdef OM_REF_DEBUG
+			trace(__FUNCTION__ " deleting %p\n", this);
+#endif
+
+			this->~OMClass();
+			fwFree(this);
+
+			return true;
+		}
+
+		return false;
+	}
+};
+
+template<typename TClass, typename... TArg>
+fxIBase* MakeNewBase(TArg... args)
+{
+	TClass* inst = new(fwAlloc(sizeof(TClass))) TClass(args...);
+	inst->AddRef();
+
+	return inst->GetBaseRef();
+}
+
+template<typename TClass, typename... TArg>
+OMPtr<TClass> MakeNew(TArg... args)
+{
+	OMPtr<TClass> retval;
+	TClass* inst = new(fwAlloc(sizeof(TClass))) TClass(args...);
+	
+	inst->AddRef();
+	*(retval.GetAddressOf()) = inst;
+
+	return retval;
+}
+}
---
+++ ObjectModel/core.h
@@ -0,0 +1,125 @@
+/*
+ * This file is part of the CitizenFX project - http://citizen.re/
+ *
+ * See LICENSE and MENTIONS in the root of the source tree for information
+ * regarding licensing.
+ */
+
+#pragma once
+
+// CFX core object model base header file.
+
+// Generic result type.
+typedef uint32_t result_t;
+
+// Common error code definitions
+#define FX_S_OK					0x0
+#define FX_E_NOTIMPL			0x80004001
+#define FX_E_NOINTERFACE		0x80004002
+#define FX_E_INVALIDARG			0x80070057
+
+// Success/failure macros
+#define FX_SUCCEEDED(x)			(((x) & 0x80000000) == 0)
+#define FX_FAILED(x)			(!FX_SUCCEEDED(x))
+
+// GUID type
+struct guid_t
+{
+	uint32_t data1;
+	uint16_t data2;
+	uint16_t data3;
+	uint8_t  data4[8];
+};
+
+// definition macro
+#define FX_DEFINE_GUID(name, d1, d2, d3, a, b, c, d, e, f, g, h) \
+	static const guid_t name = { d1, d2, d3, { a, b, c, d, e, f, g, h} };
+
+// helper functions
+namespace fx
+{
+// null GUID
+inline guid_t GetNullGuid()
+{
+	return guid_t{ 0, 0, 0, {0, 0, 0, 0, 0, 0, 0, 0} };
+}
+
+// GUID equivalence testing
+inline bool GuidEquals(const guid_t* left, const guid_t* right)
+{
+	static_assert(std::is_pod<guid_t>::value, "guid isn't a POD type");
+
+	return (memcmp(left, right, sizeof(guid_t)) == 0);
+}
+
+// GUID equivalence testing, C++ edition
+inline bool GuidEquals(const guid_t& left, const guid_t& right)
+{
+	return (memcmp(&left, &right, sizeof(guid_t)) == 0);
+}
+
+// checking for null GUID
+inline bool IsNullGuid(const guid_t& guid)
+{
+	return (GuidEquals(guid, GetNullGuid()));
+}
+}
+
+// and operators, too
+inline bool operator==(const guid_t& left, const guid_t& right)
+{
+	return (fx::GuidEquals(left, right));
+}
+
+inline bool operator!=(const guid_t& left, const guid_t& right)
+{
+	return !(left == right);
+}
+
+inline bool operator<(const guid_t& left, const guid_t& right)
+{
+	return (memcmp(&left, &right, sizeof(right)) < 0);
+}
+
+// definitions for various things used by m.o's IDL compiler
+#ifdef _MSC_VER
+#define NS_NO_VTABLE __declspec(novtable)
+#define NOINLINE __declspec(noinline)	// this is needed as the VS15.3 compiler will act weird otherwise
+#else
+#define NS_NO_VTABLE
+#define NOINLINE
+#endif
+
+#define NS_ERROR_NULL_POINTER			FX_E_INVALIDARG
+#define NS_ERROR_NOT_IMPLEMENTED		FX_E_NOTIMPL
+
+#define NS_DECLARE_STATIC_IID_ACCESSOR(iid_const) \
+	static NOINLINE inline guid_t GetIID() { return iid_const; }
+
+#ifdef _WIN32
+#define OM_DECL __stdcall
+#else
+#define OM_DECL
+#endif
+
+#define NS_IMETHOD_(rv) virtual rv OM_DECL
+#define NS_IMETHOD NS_IMETHOD_(result_t)
+
+#define NS_IMETHODIMP_(rv) virtual rv
+#define NS_IMETHODIMP NS_IMETHODIMP_(result_t)
+
+// no-op
+#define NS_DEFINE_STATIC_IID_ACCESSOR(x, y)
+
+// Start a search, returning the first class implementation matching the IID.
+extern "C" intptr_t fxFindFirstImpl(const guid_t& iid, guid_t* clsid);
+
+// Continue a fxFindFirstImpl search.
+extern "C" int32_t fxFindNextImpl(intptr_t findHandle, guid_t* clsid);
+
+// Close a fxFindFirstImpl handle.
+extern "C" void fxFindImplClose(intptr_t findHandle);
+
+// Instance creation routine
+extern "C" result_t fxCreateObjectInstance(const guid_t& guid, const guid_t& iid, void** objectRef);
+
